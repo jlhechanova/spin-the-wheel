07766e59447a1ef010a116e6f1913de2126ff04f
Jared Hechanova <jlhechanova@gmail.com>
2023-03-20 21:26:28 +0800

    fix: spins correctly after addition/deletion of items while spinning

--- src/components/Wheel.svelte
+++ src/components/Wheel.svelte
@@ -12,6 +12,7 @@
 
   $: {
     isSpinning = false;
+    arc = Math.random() * 360;
     if (canvas) {
       const ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
